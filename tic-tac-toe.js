acb26a58305f5a61989837ad46e8951f69d997e9
Daito <sha256.satoshi@gmail.com>
2024-06-17 13:14:26 +0900

    Updating index.html, style.css, and tic-tc-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,70 @@
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let isGameActive = true;
+
+const messageElement = document.getElementById('message');
+const buttonElement = document.getElementById('restartButton');
+
+const winnerConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8], 
+  [2, 4, 6]
+];
+
+function makeMove(index) {
+  if (board[index] !== '' || !isGameActive) return;
+  board[index] = currentPlayer;
+  document.getElementById(`cell-${index}`).innerHTML = currentPlayer;
+
+  helperFunction();
+}
+
+function helperFunction() {
+  let roundWon = false;
+
+  for(let i = 0; i <= 7; i++) {
+    const winCondition = winnerConditions[i];
+    let a = board[winCondition[0]];
+    let b = board[winCondition[1]];
+    let c = board[winCondition[2]];
+
+    if (a === '' || b === '' || c === '') continue;
+
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    messageElement.innerHTML =  `${currentPlayer} Wins!`;
+    isGameActive = false;
+    return;
+  }
+
+  if (!board.includes('')) {
+    messageElement.innerHTML = 'Draw';
+    isGameActive = false;
+    return;
+  }
+
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  messageElement.innerHTML = `${currentPlayer}'s Turn`;
+}
+
+function gameRestart() {
+  for(let i = 0; i <= 8; i++) {
+    document.getElementById(`cell-${i}`).innerHTML = '';
+  }
+  board = ['', '', '', '', '', '', '', '', ''];
+  currentPlayer = 'X';
+  isGameActive = true;
+  messageElement.innerHTML = `X's Turn`;
+}
+
 
